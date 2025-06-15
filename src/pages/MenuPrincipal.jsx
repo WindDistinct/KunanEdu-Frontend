c747e16f2725b698c8a7c85d9625a8d3acfcadd5
Ernesto <ernesto7816@gmail.com>
2025-06-15 01:43:56 -0500

    menu-css

--- src/pages/MenuPrincipal.jsx
+++ src/pages/MenuPrincipal.jsx
@@ -96,12 +96,11 @@ export default function MenuPrincipal() {
           <Link to="/notas" className="btn btn-accent w-full">
             Mis cursos
           </Link>
-        </div>
-         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <Link to="/nota_curso" className="btn btn-accent w-full">
+             <Link to="/nota_curso" className="btn btn-accent w-full">
             Mantenimiento Notas
           </Link>
         </div>
+         
       </section>
       )}
 
